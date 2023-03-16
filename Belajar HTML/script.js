fde27a5ad2284f686feae74fc6ddb570bf7f0dec
schoLight <215314087@student.usd.ac.id>
2023-03-16 16:26:32 +0700

    modified:   Belajar HTML/Tugas2.html 	modified:   Belajar HTML/script.js 	new file:   Belajar HTML/tugas4.html

--- Belajar HTML/script.js
+++ Belajar HTML/script.js
@@ -1,1 +1,16 @@
-// Script untuk website tugas Pemrograman Berbasis Platform
+// Membuat object for angkot
+
+function Kuliah(mahasiswa, mataKuliah, sks, biaya){
+    this.mahasiswa = mahasiswa;
+    this.mataKuliah =   mataKuliah;
+    this.sks = sks; 
+    this.biaya = biaya; 
+
+    this.tambahSks = function(mataKuliah){
+        this.sks.push(mataKuliah);
+        return  this.sks;
+    }
+}
+
+var mahasiswa = new Kuliah('Lusia Juliana', ['IMK', 'ASA', 'PAD'], [], 0);
+
